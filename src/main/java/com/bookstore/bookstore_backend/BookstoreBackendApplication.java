1dd94b53c433adaa93c37419b54a2ce25ae3f2fb
htuan2800 <huynhngoctuan48@gmail.com>
2026-09-26 04:45:46 +0700

    Khởi tạo

--- src/main/java/com/bookstore/bookstore_backend/BookstoreBackendApplication.java
+++ src/main/java/com/bookstore/bookstore_backend/BookstoreBackendApplication.java
@@ -2,9 +2,8 @@ package com.bookstore.bookstore_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class BookstoreBackendApplication {
 
 	public static void main(String[] args) {
